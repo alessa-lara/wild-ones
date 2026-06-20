87a8f1e804731cba98d3d0b3ba39aedb164a3223
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2026-06-19 23:55:39 -0300

    [feat](main) initial game logic, with treatment of user inputs

---
+++ src/main.py
@@ -0,0 +1,82 @@
+from types import MethodType
+
+from .factory import Factory_Area
+from .factory import FactoryCreature
+from .factory import FactorySpecies
+
+from .state_ages import StateMature
+
+from .area import Area
+
+def main():
+    fact_area = Factory_Area()
+    fact_spec = FactorySpecies()
+    fact_creat = FactoryCreature()
+
+    plants = []
+    creatures = []
+    area = fact_area.area(100, plants, creatures)
+
+    area_populate(fact_area, fact_spec, fact_creat, area)
+
+    inp: int = 0
+    while (inp != 1 and inp != 2):
+        area_populate(fact_area, fact_spec, fact_creat, area)
+
+        print("You wish to create another species?")
+        print("[1] Yes")
+        print("[2] No")
+        inp = int( input() )
+
+        clear_screen()
+
+    while True:
+        game(area)
+        len(area.creatures)
+
+def game(area: Area):
+    for creature in area.creatures:
+        if creature.hunger > 1:
+            creature.consume()
+        else:
+            creature.hunger += creature.hunger_rate
+
+        if creature.thirst > 1:
+            creature.consume()
+        else:
+            creature.thirst += creature.thirst_rate
+
+        if not isinstance(creature.age, StateMature):
+            continue
+
+        if creature.thirst <= 0.5 and creature.hunger <= 0.5:
+            offspring = creature.multiply()
+
+            if offspring is None:
+                continue
+
+            for child in offspring:
+                area.creatures.append(child)
+
+# creates an ansi escape sequence
+def clear_screen():
+    print("\033[H\033[J", end="")
+
+def area_populate(fact_area, fact_spec, fact_creat, area):
+    fact_spec.list_available()
+
+    spec_func: MethodType = fact_spec.select_available() # we received a function to create a valid species
+    species = spec_func(area, fact_area) 
+
+    inp: int = 0
+    while (inp <= 0):
+        print(f"How many creatures of the type {species.name} you want to create? ")
+        inp = int( input() )
+        clear_screen()
+
+    for _ in range(0, inp):
+        creature = fact_creat.creature(species)
+        area.creatures.append(creature)
+
+if __name__ == "__main__":
+    main()
